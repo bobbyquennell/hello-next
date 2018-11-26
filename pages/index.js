14771a7d98336d0f6eadfb1d86d62e56a1f5b5a0
Bobby Quennell <bobbyquennell@gmail.com>
2018-11-26 23:08:38 +1100

    support styled-components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Header from '../components/Header';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+color:red;
+font-size: 50px;
+`
 const Index = () => (
   <div>
     this is home page.
     <Header></Header>
+    <Title>My Page</Title>
   </div>
 )
 
